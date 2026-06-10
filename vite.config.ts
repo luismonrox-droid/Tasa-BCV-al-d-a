b1342c0a494a713fbfcaa54304acd975d1f28fb7
luismonrox-droid <luismonrox@gmail.com>
2026-06-10 18:11:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tasa-BCV-al-d-a/',
+  base: '/Tasa-BCV-al-d-a/',
   plugins: [react()],
 });
